3ee5ac16377b4393ad2e4dc211eaa034c2f8f89c
Gabriell-Santos <ferreirag.santos.dev@gmail.com>
2026-08-19 19:46:47 -0300

    adicionando a pagina home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,23 @@
+import style from "../../styles/home.module.css";
+import heroImg from "../assets/hero.png";
+import Image from "next/image";
 export default function Home() {
   return (
-    <div>
-      <h1>Help Taks, sistema desenvolvido para maximar suas tarefas </h1>
+    <div className={style.container}>
+      <main className={style.main}>
+        <div className={style.logo}>
+          <Image
+            className={style.LogoContent}
+            src={heroImg}
+            alt="Logo do aplicativo"
+            priority
+          />
+        </div>
+        <h1 className={style.title}>
+          Sistema feito para organizar e maximar <br />
+          as tarefas do mundo coorporativo
+        </h1>
+      </main>
     </div>
   );
 }
