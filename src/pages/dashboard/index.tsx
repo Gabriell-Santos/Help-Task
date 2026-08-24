74ef6ad4fea68b9dab1bd9c45a041b9a04578001
Gabriell-Santos <ferreirag.santos.dev@gmail.com>
2026-08-24 12:57:06 -0300

    protegendo a rota do dashboard

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,5 +1,8 @@
 import Head from "next/head";
 import style from "./style.module.css";
+import { getSession } from "next-auth/react";
+import { GetServerSideProps } from "next";
+import { redirect } from "next/dist/server/api-utils";
 export default function Dashboard() {
   return (
     <div className={style.container}>
@@ -10,3 +13,23 @@ export default function Dashboard() {
     </div>
   );
 }
+
+// Coloco em baixo a busca dos dados vindo do servidor
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+  const session = await getSession({ req });
+
+  // Protegendo a rota
+  if (!session?.user) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+
+  // Dentro do getServerSideProps sempre tem que retorna algo
+  return {
+    props: {},
+  };
+};
